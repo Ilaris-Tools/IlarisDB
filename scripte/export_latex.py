9399cd029dc2efbed57dab38e1ac6358c3d958dd
Lukas R <mail@sarbot.de>
2022-02-15 13:55:43 +0100

    tex export an elefant getestet

--- scripte/export_latex.py
+++ scripte/export_latex.py
@@ -6,18 +6,56 @@ textemplate = Template("""
     $body
 }}""")
 
+
 output = ""
+
+
+kampfwertkeys = {
+    "MR": "Magieresistenz",
+    "WS": "Wundschwelle",
+    "GS": "Geschwindigkeit",
+    "INI": "Initiative",
+}  # todo: in eigene yaml file verschieben?
+
 
 with open("../daten/kreaturen.yml", "r") as kfile:
     kreaturen = yaml.load(kfile)
 for id, k in kreaturen.items():
-    # die zeilen sind optional daher erstmal manuell durchgehen, vlt. spaeter in elegant
-    body = ""
+    bloecke = []
     if "kampfwerte" in k:
-        nl = '\\\\'
-        body += f"\kreaturinfo{{Kampfwerte}}{{{nl.join([str(x) for x in k['kampfwerte'].items()])}}}"
+        w = k['kampfwerte']
+        bloecke.append(
+            f"\\kreaturkampfwerte{{{w['WS']}}}{{{w['MR']}}}{{{w['GS']}}}{{{w['INI']}}}")
+    if "vorteile" in k:
+        # ohne weitere formatierung
+        bloecke.append(f"\\kreaturvorteile{{{', '.join(k['vorteile'])}}}")
+    if "waffen" in k or "kampfvorteile" in k:
+        block = ""
+        if "waffen" in k:
+            for w in k['waffen']:
+                block += f"\\kreaturwaffe{{{w['name']}}}{{{w.get('RW', '')}}}{{{w.get('VT','')}}}{{{w.get('AT','')}}}{{{w.get('TP','')}}}{{{w.get('bemerkung', '')}}}"
+        if "kampfvorteile" in k:
+            block += f"\\kreaturkampfvorteile{{{', '.join(k['kampfvorteile'])}}}"
+        bloecke.append(block)
+    if "fertigkeiten" in k or "attribute" in k:
+        block = ""
+        if "attribute" in k:
+            attr = ", ".join([f"{a} {v}" for a, v in k['attribute'].items()])
+            block += f"\\kreaturattribute{{{attr}}}"
+        if "fertigkeiten" in k:
+            fert = ", ".join(
+                [f"{a} {v}" for a, v in k['fertigkeiten'].items()])
+            block += f"\\kreaturfertigkeiten{{{fert}}}"
+        bloecke.append(block)
+    if "varianten" in k:
+        varn = '\\\\'.join(k['varianten'])
+        bloecke.append(f"\\kreaturvarianten{{{varn}}}")
 
     output += textemplate.substitute(
-        id=id, name=k['name'], klasse=k.get('klasse', 'humanoid'), body=body, beschreibung=k.get('beschreibung'))
+        id=id,
+        name=k['name'],
+        klasse=k.get('klasse', 'humanoid'),
+        body="\\trennlinie".join(bloecke),
+        beschreibung=k.get('beschreibung'))
 with open("../export/latex/kreaturen.tex", "w") as tfile:
     tfile.write(output)
